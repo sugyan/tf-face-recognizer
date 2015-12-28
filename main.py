3fae29a50c481690996c138aa8a7e8e2ff21dd0c
sugyan <sugi1982@gmail.com>
2015-12-29 01:25:43 +0900

    update

--- main.py
+++ main.py
@@ -3,44 +3,58 @@ import tensorflow as tf
 
 cifar10.IMAGE_SIZE = 96
 cifar10.NUM_CLASSES = 6
-cifar10.FLAGS.batch_size = 1
+cifar10.FLAGS.batch_size = 10
 
-label = tf.cast(tf.slice(tf.decode_raw("\x01", tf.uint8), [0], [1]), tf.int32)
+def inputs():
+    # target files
+    filenames = []
+    with open('./data/filelist.txt', 'r') as f:
+        for line in f:
+            filenames.append(line.strip())
+    fqueue = tf.train.string_input_producer(filenames)
 
-sess = tf.Session()
-with sess.as_default():
-    image = tf.image.decode_jpeg(open('./image', 'r').read()).eval()
-image = tf.cast(image, tf.float32)
+    # read and parse from file queue
+    depth = 3
+    label_bytes = 1
+    image_bytes = cifar10.IMAGE_SIZE * cifar10.IMAGE_SIZE * depth
+    record_bytes = image_bytes + label_bytes
+    reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
+    key, value = reader.read(fqueue)
+    record_bytes = tf.decode_raw(value, tf.uint8)
+    # The first bytes represent the label, which we convert from uint8->int32.
+    label = tf.cast(tf.slice(record_bytes, [0], [label_bytes]), tf.int32)
+    # The remaining bytes after the label represent the image, which we reshape to [height, width, depth].
+    uint8image = tf.reshape(
+        tf.slice(record_bytes, [label_bytes], [image_bytes]),
+        [cifar10.IMAGE_SIZE, cifar10.IMAGE_SIZE, depth]
+    )
+    image = tf.cast(uint8image, tf.float32)
+    return image, label
 
-images, label_batch = tf.train.shuffle_batch(
-    [image, label],
-    batch_size = cifar10.FLAGS.batch_size,
-    capacity = 3 * cifar10.FLAGS.batch_size,
-    min_after_dequeue = 1
-)
-labels = tf.reshape(label_batch, [cifar10.FLAGS.batch_size])
+def train():
+    image, label = inputs()
+    images, label_batch = tf.train.shuffle_batch(
+        [image, label],
+        batch_size = cifar10.FLAGS.batch_size,
+        capacity = 3 * cifar10.FLAGS.batch_size,
+        min_after_dequeue = 1
+    )
+    labels = tf.reshape(label_batch, [cifar10.FLAGS.batch_size])
+    logits = cifar10.inference(images)
 
-# images, labels = cifar10.inputs(eval_data='test')
-# print images
+    with tf.Session() as sess:
+        init = tf.initialize_all_variables()
+        sess.run(init)
 
-logits = cifar10.inference(images)
-for variable in tf.all_variables():
-    print variable.name
-
-with tf.Session() as sess:
-    sess.run(tf.initialize_all_variables())
-
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-    try:
-        # while not coord.should_stop():
-            # Run training steps or whatever
-        print sess.run(logits)
-    except tf.errors.OutOfRangeError:
-        print('Done training -- epoch limit reached')
-    finally:
-        # When done, ask the threads to stop.
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        try:
+            print sess.run(logits)
+            # TODO
+        except Exception as e:
+            coord.request_stop(e)
         coord.request_stop()
+        coord.join(threads)
 
-    coord.request_stop()
-    coord.join(threads)
+if __name__ == '__main__':
+    train()
