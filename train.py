edeb1fc460385f4484934a8a90b9db5be654e8d3
sugyan <sugi1982@gmail.com>
2016-01-01 23:17:09 +0900

    update train.py

--- train.py
+++ train.py
@@ -5,26 +5,20 @@ import os
 
 cifar10.IMAGE_SIZE = 48
 cifar10.NUM_CLASSES = 6
-# cifar10.FLAGS.batch_size = 64
-cifar10.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 1000
-# cifar10.NUM_EPOCHS_PER_DECAY = 1000.0
-# cifar10.LEARNING_RATE_DECAY_FACTOR = 0.02
-cifar10.INITIAL_LEARNING_RATE = 0.05
+cifar10.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 250
+cifar10.NUM_EPOCHS_PER_DECAY = 1000.0
+# cifar10.INITIAL_LEARNING_RATE = 0.09
 
 def inputs():
-    # target files
+    # read from target files
     filenames = []
     with open('./data/filelist.txt', 'r') as f:
         for line in f:
             filenames.append(line.strip())
     fqueue = tf.train.string_input_producer(filenames)
 
-    # read and parse from file queue
-    height = 96
-    width = 96
-    depth = 3
     label_bytes = 1
-    image_bytes = height * width * depth
+    image_bytes = 48 * 48 * 3
     record_bytes = image_bytes + label_bytes
     reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
     key, value = reader.read(fqueue)
@@ -32,64 +26,59 @@ def inputs():
     # The first bytes represent the label, which we convert from uint8->int32.
     label = tf.cast(tf.slice(record_bytes, [0], [label_bytes]), tf.int32)
     # The remaining bytes after the label represent the image, which we reshape to [height, width, depth].
-    uint8image = tf.reshape(
-        tf.slice(record_bytes, [label_bytes], [image_bytes]),
-        [height, width, depth]
-    )
-    image = tf.cast(uint8image, tf.float32)
-    return image, label
-
-def train():
-    checkpoint_dir = 'train'
-    checkpoint_path = os.path.join(checkpoint_dir, 'model.ckpt')
-
-    image, label = inputs()
-    images, label_batch = tf.train.shuffle_batch(
+    uint8image = tf.reshape(tf.slice(record_bytes, [label_bytes], [image_bytes]), [3, 48, 48])
+    image = tf.transpose(tf.cast(uint8image, tf.float32), [1, 2, 0])
+    return tf.train.shuffle_batch(
         [image, label],
         batch_size = cifar10.FLAGS.batch_size,
         capacity = 3 * cifar10.FLAGS.batch_size,
         min_after_dequeue = 1
     )
-    labels = tf.reshape(label_batch, [cifar10.FLAGS.batch_size])
 
+def train(checkpoint_dir):
+    # ops
+    global_step = tf.Variable(0, trainable=False)
+    images, labels = inputs()
     logits = cifar10.inference(tf.image.resize_images(images, cifar10.IMAGE_SIZE, cifar10.IMAGE_SIZE))
     loss = cifar10.loss(logits, labels)
-    global_step = tf.Variable(0, trainable=False)
     train_op = cifar10.train(loss, global_step)
-    saver = tf.train.Saver(tf.all_variables())
+    summary_op = tf.merge_all_summaries()
 
     with tf.Session() as sess:
-        prev_ckpt = None
-        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
-        if ckpt and ckpt.model_checkpoint_path:
-            prev_ckpt = ckpt.model_checkpoint_path
-            saver.restore(sess, ckpt.model_checkpoint_path)
-        else:
-            init = tf.initialize_all_variables()
-            sess.run(init)
+        saver = tf.train.Saver(tf.all_variables())
+        checkpoint_path = os.path.join(checkpoint_dir, 'model.ckpt')
+        summary_writer = tf.train.SummaryWriter(checkpoint_dir, graph_def=sess.graph_def)
+        if not tf.train.get_checkpoint_state(checkpoint_dir):
+            sess.run(tf.initialize_all_variables())
+            saver.save(sess, checkpoint_path, global_step=0)
 
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord)
-        try:
+        tf.train.start_queue_runners(sess=sess)
+        while True:
+            # restore or initialize variables
+            ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+            if ckpt.model_checkpoint_path:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+
             loss_values = []
-            for i in range(10):
+            # train
+            for step in range(100):
                 start_time = time.time()
                 _, loss_value, global_step_value = sess.run([train_op, loss, global_step])
+                duration = time.time() - start_time
+                print '%d: %f (%.3f sec/batch)' % (global_step_value, loss_value, duration)
+                loss_values.append(loss_value)
                 if loss_value > 1e6:
                     break
-                loss_values.append(loss_value)
-                duration = time.time() - start_time
-                print '%3d: %f (%.3f sec/batch)' % (global_step_value, loss_value, duration)
-            if loss_values[0] > loss_values[-1]:
-                if prev_ckpt is not None:
-                    os.remove(prev_ckpt)
-                saver.save(sess, checkpoint_path, global_step=global_step_value)
-            else:
-                print 'train failed!'
-        except Exception as e:
-            coord.request_stop(e)
-        coord.request_stop()
-        coord.join(threads)
+            if not loss_values[0] > loss_values[-1]:
+                print 'failed.'
+                continue
+            # save and remove old checkpoint
+            saver.save(sess, checkpoint_path, global_step=global_step_value)
+            os.remove(ckpt.model_checkpoint_path)
+            # add summary
+            if global_step_value % 1000 == 0:
+                summary_str = sess.run(summary_op)
+                summary_writer.add_summary(summary_str, global_step_value)
 
 def print_parameters():
     print '''
@@ -108,4 +97,4 @@ def print_parameters():
 
 if __name__ == '__main__':
     print_parameters()
-    train()
+    train(os.path.join(os.path.dirname(__file__), 'train'))
