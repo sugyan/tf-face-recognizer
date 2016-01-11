699fc47f98a7a22fcb996398aec63edb0864d33b
sugyan <sugi1982@gmail.com>
2016-01-12 00:08:57 +0900

    multidict request

--- web.py
+++ web.py
@@ -30,17 +30,19 @@ app.debug = True
 
 @app.route('/', methods=['POST'])
 def api():
-    image = request.form['image']
-    data = base64.b64decode(image.split(',')[1])
-    if image.startswith('data:image/jpeg;base64,'):
-        decoded = tf.image.decode_jpeg(data, channels=3)
-    if image.startswith('data:image/png;base64,'):
-        decoded = tf.image.decode_png(data, channels=3)
-    inputs = tf.reshape(decoded, decoded.eval(session=tf.Session()).shape)
-    inputs = tf.image.per_image_whitening(inputs)
-    inputs = tf.image.resize_images(tf.expand_dims(inputs, 0), FLAGS.image_size, FLAGS.image_size)
-    output = sess.run(logits, feed_dict={images: inputs.eval(session=tf.Session())})
-    return jsonify(result=output.flatten().tolist())
+    results = []
+    for image in request.form.getlist('images'):
+        data = base64.b64decode(image.split(',')[1])
+        if image.startswith('data:image/jpeg;base64,'):
+            decoded = tf.image.decode_jpeg(data, channels=3)
+        if image.startswith('data:image/png;base64,'):
+            decoded = tf.image.decode_png(data, channels=3)
+        inputs = tf.reshape(decoded, decoded.eval(session=tf.Session()).shape)
+        inputs = tf.image.per_image_whitening(inputs)
+        inputs = tf.image.resize_images(tf.expand_dims(inputs, 0), FLAGS.image_size, FLAGS.image_size)
+        output = sess.run(logits, feed_dict={images: inputs.eval(session=tf.Session())})
+        results.append(output.flatten().tolist())
+    return jsonify(results=results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=FLAGS.port)
