5cf7f5e3649b4c765af383096e43208242062e1e
sugyan <sugi1982@gmail.com>
2016-01-18 22:23:45 +0900

    v2: update parameters, add summary and histograms

--- models/v2.py
+++ models/v2.py
@@ -57,36 +57,44 @@ def inference(images):
             tf.add_to_collection('losses', weight_decay)
         return var
 
+    def _activation_summary(x):
+        tensor_name = x.op.name
+        tf.scalar_summary(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
+
     with tf.variable_scope('conv1') as scope:
-        kernel = tf.get_variable('weights', shape=[3, 3, 3, 32], initializer=tf.truncated_normal_initializer(stddev=1e-1))
+        kernel = tf.get_variable('weights', shape=[3, 3, 3, 32], initializer=tf.truncated_normal_initializer(stddev=0.1))
         conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
         biases = tf.get_variable('biases', shape=[32], initializer=tf.constant_initializer(0.0))
         bias = tf.nn.bias_add(conv, biases)
         conv1 = tf.nn.relu(bias, name=scope.name)
+        _activation_summary(conv1)
     pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool1')
 
     with tf.variable_scope('conv2') as scope:
-        kernel = tf.get_variable('weights', shape=[3, 3, 32, 64], initializer=tf.truncated_normal_initializer(stddev=1e-1))
+        kernel = tf.get_variable('weights', shape=[3, 3, 32, 64], initializer=tf.truncated_normal_initializer(stddev=0.1))
         conv = tf.nn.conv2d(pool1, kernel, [1, 1, 1, 1], padding='SAME')
         biases = tf.get_variable('biases', shape=[64], initializer=tf.constant_initializer(0.0))
         bias = tf.nn.bias_add(conv, biases)
         conv2 = tf.nn.relu(bias, name=scope.name)
+        _activation_summary(conv2)
     pool2 = tf.nn.max_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool2')
 
     with tf.variable_scope('conv3') as scope:
-        kernel = tf.get_variable('weights', shape=[3, 3, 64, 128], initializer=tf.truncated_normal_initializer(stddev=1e-1))
+        kernel = tf.get_variable('weights', shape=[3, 3, 64, 128], initializer=tf.truncated_normal_initializer(stddev=0.1))
         conv = tf.nn.conv2d(pool2, kernel, [1, 1, 1, 1], padding='SAME')
         biases = tf.get_variable('biases', shape=[128], initializer=tf.constant_initializer(0.0))
         bias = tf.nn.bias_add(conv, biases)
         conv3 = tf.nn.relu(bias, name=scope.name)
+        _activation_summary(conv3)
     pool3 = tf.nn.max_pool(conv3, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool3')
 
     with tf.variable_scope('conv4') as scope:
-        kernel = tf.get_variable('weights', shape=[3, 3, 128, 256], initializer=tf.truncated_normal_initializer(stddev=1e-1))
+        kernel = tf.get_variable('weights', shape=[3, 3, 128, 256], initializer=tf.truncated_normal_initializer(stddev=0.1))
         conv = tf.nn.conv2d(pool3, kernel, [1, 1, 1, 1], padding='SAME')
         biases = tf.get_variable('biases', shape=[256], initializer=tf.constant_initializer(0.0))
         bias = tf.nn.bias_add(conv, biases)
         conv4 = tf.nn.relu(bias, name=scope.name)
+        _activation_summary(conv4)
     pool4 = tf.nn.max_pool(conv4, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool4')
 
     with tf.variable_scope('fc5') as scope:
@@ -94,19 +102,22 @@ def inference(images):
         for d in pool4.get_shape()[1:].as_list():
             dim *= d
         reshape = tf.reshape(pool4, [BATCH_SIZE, dim])
-        weights = _variable_with_weight_decay('weights', shape=[dim, 1024], stddev=0.01, wd=0.001)
-        biases = tf.get_variable('biases', shape=[1024], initializer=tf.constant_initializer(0.1))
-    fc5 = tf.nn.relu(tf.nn.bias_add(tf.matmul(reshape, weights), biases), name=scope.name)
+        weights = _variable_with_weight_decay('weights', shape=[dim, 1024], stddev=0.01, wd=0.005)
+        biases = tf.get_variable('biases', shape=[1024], initializer=tf.constant_initializer(0.0))
+        fc5 = tf.nn.relu(tf.nn.bias_add(tf.matmul(reshape, weights), biases), name=scope.name)
+        _activation_summary(fc5)
 
     with tf.variable_scope('fc6') as scope:
-        weights = _variable_with_weight_decay('weights', shape=[1024, 256], stddev=0.01, wd=0.001)
-        biases = tf.get_variable('biases', shape=[256], initializer=tf.constant_initializer(0.1))
-    fc6 = tf.nn.relu(tf.nn.bias_add(tf.matmul(fc5, weights), biases), name=scope.name)
+        weights = _variable_with_weight_decay('weights', shape=[1024, 256], stddev=0.01, wd=0.005)
+        biases = tf.get_variable('biases', shape=[256], initializer=tf.constant_initializer(0.0))
+        fc6 = tf.nn.relu(tf.nn.bias_add(tf.matmul(fc5, weights), biases), name=scope.name)
+        _activation_summary(fc6)
 
     with tf.variable_scope('fc7') as scope:
         weights = tf.get_variable('weights', shape=[256, NUM_CLASSES], initializer=tf.truncated_normal_initializer(stddev=1/256.0))
-        biases = tf.get_variable('biases', shape=[NUM_CLASSES], initializer=tf.constant_initializer(0.1))
-    fc7 = tf.nn.relu(tf.nn.bias_add(tf.matmul(fc6, weights), biases), name=scope.name)
+        biases = tf.get_variable('biases', shape=[NUM_CLASSES], initializer=tf.constant_initializer(0.0))
+        fc7 = tf.nn.bias_add(tf.matmul(fc6, weights), biases, name=scope.name)
+        _activation_summary(fc7)
 
     return fc7
 
@@ -129,11 +140,17 @@ def train(total_loss, global_step):
     for l in losses + [total_loss]:
         tf.scalar_summary(l.op.name + ' (raw)', l)
 
-    # Apply gradients
+    # Apply gradients, and add histograms
     with tf.control_dependencies([loss_averages_op]):
         opt = tf.train.AdamOptimizer()
         grads = opt.compute_gradients(total_loss)
     apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+    for var in tf.trainable_variables():
+        tf.histogram_summary(var.op.name, var)
+    for grad, var in grads:
+        if grad:
+            tf.histogram_summary(var.op.name + '/gradients', grad)
+
     # Track the moving averages of all trainable variables
     variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
     variables_averages_op = variable_averages.apply(tf.trainable_variables())
