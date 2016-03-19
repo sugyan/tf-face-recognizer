127a70b121fa6795bc364e9623285fe2f0e59a14
sugyan <sugi1982@gmail.com>
2016-03-19 16:09:13 +0900

    oops..

--- data/v2/download.py
+++ data/v2/download.py
@@ -38,7 +38,7 @@ with open(os.path.join(os.path.dirname(__file__), 'tfrecords', 'labels.json'), '
 samples = 0
 for target in targets:
     samples += target['sample']
-    params = urllib.urlencode({ 'sample': 100 if target['sample'] > 100 else target['sample'] })
+    params = urllib.urlencode({ 'sample': 100 if target['index'] > 0 and target['sample'] > 100 else target['sample'] })
     url = url_base + '/faces/tfrecords/%d?%s' % (target['index'], params)
     filename = os.path.join(os.path.dirname(__file__), 'tfrecords', '%03d.tfrecords' % target['index'])
     print urllib.urlretrieve(url, filename)
