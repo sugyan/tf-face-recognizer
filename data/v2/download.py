4ce87537774b9e277166cd15cff3bf6ab335878a
sugyan <sugi1982@gmail.com>
2016-03-29 11:19:08 +0900

    revert changes

--- data/v2/download.py
+++ data/v2/download.py
@@ -21,10 +21,11 @@ samples = 0
 for label in json.loads(urllib.urlopen(url).read()):
     index_number = label['index_number']
     if index_number is not None:
-        samples += label['faces_count']
+        sample = 100 if index_number > 0 and label['faces_count'] > 100 else label['faces_count']
+        samples += sample
         targets.append({
             'index': index_number,
-            'sample': label['faces_count']
+            'sample': sample
         })
         labels[index_number] = label
 targets.append({
