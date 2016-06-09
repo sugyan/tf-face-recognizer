8c0095c006b3ee70e89e4af6cd429b3b99676b06
sugyan <sugi1982@gmail.com>
2016-06-09 20:12:01 +0900

    wip

---
+++ eval/visualize.py
@@ -0,0 +1,37 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+os.environ['BATCH_SIZE'] = '100'
+
+import model
+import tensorflow as tf
+from eval import inputs
+
+FLAGS = tf.app.flags.FLAGS
+
+def main(argv=None):
+    filepath = os.path.join(FLAGS.data_dir, 'data-00.tfrecords')
+    images, labels = inputs([filepath])
+    logits = model.inference(images, FLAGS.num_classes)
+    saver = tf.train.Saver(tf.all_variables())
+
+    checkpoint = tf.train.latest_checkpoint(FLAGS.train_dir)
+    with tf.Session() as sess:
+        saver.restore(sess, checkpoint)
+
+        targets = [
+            {'name': 'conv1/conv1:0'},
+            {'name': 'conv2/conv2:0'},
+            {'name': 'conv3/conv3:0'},
+            {'name': 'conv4/conv4:0'},
+            {'name': 'fc5/fc5:0'},
+            {'name': 'fc6/fc6:0'},
+            {'name': 'fc7/fc7:0'},
+        ]
+        for target in targets:
+            t = sess.graph.get_tensor_by_name(target['name'])
+            print(t)
+
+
+if __name__ == '__main__':
+    tf.app.run()
