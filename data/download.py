ee866f1db5d19c88cfb117e14f90adfd4640d603
sugyan <sugi1982@gmail.com>
2016-06-18 00:21:51 +0900

    update download.py for python3

--- data/download.py
+++ data/download.py
@@ -1,9 +1,17 @@
 import logging
-import urllib
 import random
 import sys
 import os
 import json
+import codecs
+
+try:
+    from urllib.parse import urlencode
+    from urllib.request import urlopen
+except ImportError:
+    from urllib import urlencode
+    from urllib2 import urlopen
+
 
 url_base = sys.argv[1]
 
@@ -17,7 +25,9 @@ for f in os.listdir(data_dir):
 targets, labels = [], {}
 url = url_base + '/labels.json'
 samples = 0
-for label in json.loads(urllib.urlopen(url).read()):
+
+reader = codecs.getreader('utf-8')
+for label in json.load(reader(urlopen(url))):
     index_number = label['index_number']
     if index_number is not None:
         sample = 120 if index_number > 0 and label['faces_count'] > 120 else label['faces_count']
@@ -29,7 +39,7 @@ for label in json.loads(urllib.urlopen(url).read()):
         labels[index_number] = label
 targets.append({
     'index': 0,
-    'sample': samples / 3
+    'sample': int(samples / 3)
 })
 
 # labels data
@@ -37,17 +47,17 @@ with open(os.path.join(os.path.dirname(__file__), 'tfrecords', 'labels.json'), '
     f.write(json.dumps(labels))
 # download source data
 for target in targets:
-    url = url_base + '/faces/tfrecords/%d?%s' % (target['index'], urllib.urlencode({ 'sample': target['sample'] }))
+    url = url_base + '/faces/tfrecords/%d?%s' % (target['index'], urlencode({ 'sample': target['sample'] }))
     number = 0 if target['index'] == 0 else (target['index'] - 1) / 10 + 1
     filename = os.path.join(data_dir, '%02d.tfrecords' % number)
     if target['sample'] <= 60:
         with open(filename, 'ab') as f:
-            f.write(urllib.urlopen(url).read())
-            f.write(urllib.urlopen(url).read())
-        print '%s (%d: %d x2)' % (filename, target['index'], target['sample'])
+            f.write(urlopen(url).read())
+            f.write(urlopen(url).read())
+        print('%s (%d: %d x2)' % (filename, target['index'], target['sample']))
     else:
         with open(filename, 'ab') as f:
-            f.write(urllib.urlopen(url).read())
-        print '%s (%d: %d)' % (filename, target['index'], target['sample'])
+            f.write(urlopen(url).read())
+        print('%s (%d: %d)' % (filename, target['index'], target['sample']))
 
-print samples + samples / 4
+print(samples + int(samples / 3))
