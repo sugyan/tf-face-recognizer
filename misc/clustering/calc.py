d2552303049d5ab0938e33864368fca1d13a0fdd
sugyan <sugi1982@gmail.com>
2016-10-19 21:24:48 +0900

    draw dendrogram

--- misc/clustering/calc.py
+++ misc/clustering/calc.py
@@ -1,11 +1,12 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 from scipy.spatial.distance import pdist
-from scipy.cluster.hierarchy import linkage
+from scipy.cluster.hierarchy import linkage, dendrogram
 
 target = 'fc5.csv'
 df = pd.read_csv(os.path.join(os.path.dirname(__file__), target), index_col=0, header=None)
 row_clusters = linkage(pdist(df, metric='euclidean'), method='complete')
 
-cl0 = row_clusters[0]
-print(df.index[cl0[0]], df.index[cl0[1]])
+row_dendr = dendrogram(row_clusters, labels=df.index, orientation='right')
+plt.show()
