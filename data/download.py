342bf2c4e881041864d50e88169e579023fbb096
sugyan <sugi1982@gmail.com>
2016-11-14 20:35:35 +0900

    update downloader for authentication

--- data/download.py
+++ data/download.py
@@ -7,13 +7,17 @@ import codecs
 
 try:
     from urllib.parse import urlencode
-    from urllib.request import urlopen
+    from urllib.request import urlopen, Request
 except ImportError:
     from urllib import urlencode
-    from urllib2 import urlopen
+    from urllib2 import urlopen, Request
 
 
 url_base = sys.argv[1]
+auth_headers = {
+    'X-User-Email': os.environ['API_AUTH_EMAIL'],
+    'X-User-Token': os.environ['API_AUTH_TOKEN'],
+}
 
 # remove old records
 data_dir = os.path.join(os.path.dirname(__file__), 'tfrecords')
@@ -24,10 +28,11 @@ for f in os.listdir(data_dir):
 # config
 targets, labels = [], {}
 url = url_base + '/labels.json?include_counts=true'
+req = Request(url, None, auth_headers)
 samples = 0
 
 reader = codecs.getreader('utf-8')
-for label in json.load(reader(urlopen(url))):
+for label in json.load(reader(urlopen(req))):
     index_number = label['index_number']
     if index_number is not None:
         sample = 120 if index_number > 0 and label['faces_count'] > 120 else label['faces_count']
@@ -48,16 +53,17 @@ with open(os.path.join(os.path.dirname(__file__), 'tfrecords', 'labels.json'), '
 # download source data
 for target in targets:
     url = url_base + '/faces/tfrecords/%d?%s' % (target['index'], urlencode({ 'sample': target['sample'] }))
+    req = Request(url, None, auth_headers)
     number = 0 if target['index'] == 0 else (target['index'] - 1) / 10 + 1
     filename = os.path.join(data_dir, '%02d.tfrecords' % number)
     if target['sample'] <= 60:
         with open(filename, 'ab') as f:
-            f.write(urlopen(url).read())
-            f.write(urlopen(url).read())
+            f.write(urlopen(req).read())
+            f.write(urlopen(req).read())
         print('%s (%d: %d x2)' % (filename, target['index'], target['sample']))
     else:
         with open(filename, 'ab') as f:
-            f.write(urlopen(url).read())
+            f.write(urlopen(req).read())
         print('%s (%d: %d)' % (filename, target['index'], target['sample']))
 
 print(samples + int(samples / 3))
