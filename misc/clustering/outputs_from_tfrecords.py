9702baecfbcf5ad40c1120b438272410cca248f9
sugyan <sugi1982@gmail.com>
2017-02-03 02:41:45 +0900

    update

--- misc/clustering/outputs_from_tfrecords.py
+++ misc/clustering/outputs_from_tfrecords.py
@@ -5,8 +5,8 @@ import tensorflow as tf
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('model_path', '/tmp/model.pb',
                            """Directory where to read model data.""")
-tf.app.flags.DEFINE_integer('input_size', 96,
-                            """Size of input image""")
+tf.app.flags.DEFINE_string('file', 'data.tfrecords',
+                           """Path to the tfrecord file.""")
 
 
 def main(argv=None):
@@ -18,48 +18,32 @@ def main(argv=None):
     with tf.gfile.FastGFile(FLAGS.model_path, 'rb') as f:
         graph_def.ParseFromString(f.read())
     tf.import_graph_def(graph_def, name='')
-
+    # fc5, fc6
     graph = tf.get_default_graph()
     fc5 = graph.get_tensor_by_name('fc5/fc5:0')
     fc6 = graph.get_tensor_by_name('fc6/fc6:0')
-    print(fc5, fc6)
 
-    # r = Recognizer(batch_size=1)
-    # data = tf.placeholder(tf.string)
-    # image = tf.image.decode_jpeg(data, channels=3)
-    # image = tf.image.resize_image_with_crop_or_pad(image, FLAGS.input_size, FLAGS.input_size)
-    # image = tf.image.per_image_standardization(image)
-    # inputs = tf.expand_dims(image, axis=0)
-    # r.inference(inputs, 0)
-    # fc5 = tf.get_default_graph().get_tensor_by_name('fc5/fc5:0')
-    # fc6 = tf.get_default_graph().get_tensor_by_name('fc6/fc6:0')
-
-    # with tf.Session() as sess:
-    #     variable_averages = tf.train.ExponentialMovingAverage(r.MOVING_AVERAGE_DECAY)
-    #     variables_to_restore = variable_averages.variables_to_restore()
-    #     for name, v in variables_to_restore.items():
-    #         try:
-    #             tf.train.Saver([v]).restore(sess, FLAGS.checkpoint_path)
-    #         except Exception:
-    #             print('initialize %s' % name)
-    #             sess.run(tf.variables_initializer([v]))
-
-    #     outputs = {}
-    #     dirname = os.path.join(os.path.dirname(__file__), 'images')
-    #     for filename in os.listdir(dirname):
-    #         if not filename.endswith('.jpg'):
-    #             continue
-    #         with open(os.path.join(dirname, filename), 'rb') as f:
-    #             results = sess.run({'fc5': fc5, 'fc6': fc6}, feed_dict={data: f.read()})
-    #         outputs[filename] = {
-    #             'fc5': results['fc5'].flatten().tolist(),
-    #             'fc6': results['fc6'].flatten().tolist(),
-    #         }
-    # for out in ['fc5', 'fc6']:
-    #     filename = os.path.join(os.path.dirname(__file__), '%s.csv' % out)
-    #     with open(filename, 'w') as f:
-    #         for name, values in outputs.items():
-    #             f.write(','.join([name] + [str(x) for x in values[out]]) + '\n')
+    example = tf.placeholder(tf.string)
+    features = tf.parse_single_example(example, features={
+        'image_raw': tf.FixedLenFeature([], tf.string)
+    })
+    with tf.Session() as sess:
+        # read records and run for getting outputs
+        outputs = {}
+        for i, record in enumerate(tf.python_io.tf_record_iterator(FLAGS.file)):
+            print('processing {:04d}...'.format(i))
+            image = sess.run(features['image_raw'], feed_dict={example: record})
+            results = sess.run({'fc5': fc5, 'fc6': fc6}, feed_dict={'contents:0': image})
+            outputs[i] = {
+                'fc5': results['fc5'].flatten().tolist(),
+                'fc6': results['fc6'].flatten().tolist(),
+            }
+    # write outputs to CSV
+    for out in ['fc5', 'fc6']:
+        filename = os.path.join(os.path.dirname(__file__), '{}.csv'.format(out))
+        with open(filename, 'w') as f:
+            for name, values in outputs.items():
+                f.write(','.join(['{:04d}'.format(name)] + [str(x) for x in values[out]]) + '\n')
 
 
 if __name__ == '__main__':
