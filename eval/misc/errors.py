6dde3212a053a8b6e23a675719777b34a6e52407
sugyan <sugi1982@gmail.com>
2017-02-22 02:54:52 +0900

    update misc

--- eval/misc/errors.py
+++ eval/misc/errors.py
@@ -21,15 +21,32 @@ def main(argv=None):
         'image_raw': tf.FixedLenFeature([], tf.string)
     })
     decode = tf.image.decode_jpeg(features['image_raw'], channels=3)
-    image = tf.image.resize_image_with_crop_or_pad(decode, 96, 96)
-    inputs = tf.expand_dims(tf.image.per_image_standardization(image), axis=0)
+    image1 = tf.image.resize_image_with_crop_or_pad(decode, 88, 88)
+    image1 = tf.image.resize_images(image1, [96, 96])
+    image2 = tf.image.resize_image_with_crop_or_pad(decode, 96, 96)
+    image2 = tf.image.resize_images(image2, [96, 96])
+    image3 = tf.image.resize_image_with_crop_or_pad(decode, 104, 104)
+    image3 = tf.image.resize_images(image3, [96, 96])
+    image4 = tf.image.resize_image_with_crop_or_pad(decode, 100, 100)
+    image4 = tf.image.resize_images(image4, [96, 96])
+    image5 = tf.image.resize_image_with_crop_or_pad(decode, 92, 92)
+    image5 = tf.image.resize_images(image5, [96, 96])
+    inputs = tf.stack([
+        tf.image.per_image_standardization(image1),
+        tf.image.per_image_standardization(image2),
+        tf.image.per_image_standardization(image3),
+        tf.image.per_image_standardization(image4),
+        tf.image.per_image_standardization(image5)
+    ])
+    # inputs = tf.expand_dims(tf.image.per_image_standardization(image), axis=0)
     labels = tf.expand_dims(features['label'], axis=0)
 
     sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
     import model
     logits = tf.nn.softmax(model.inference(inputs, FLAGS.num_classes))
-    _, indices = tf.nn.top_k(logits)
-    top = indices[0]
+    values, indices = tf.nn.top_k(logits)
+    _, top_value = tf.nn.top_k(tf.transpose(values))
+    answer = tf.gather(indices, tf.squeeze(top_value))
     # variable_averages = tf.train.ExponentialMovingAverage(model.MOVING_AVERAGE_DECAY)
 
     with tf.Session() as sess:
@@ -40,12 +57,11 @@ def main(argv=None):
         errors = {}
         error_images = []
         for i, record in enumerate(tf.python_io.tf_record_iterator(FLAGS.data_file)):
-            top_value, labels_value, image_value = sess.run([top, labels, image], feed_dict={example: record})
-            if top_value[0] == labels_value[0]:
+            labels_value, answer_value, image_value = sess.run([labels, answer, image2], feed_dict={example: record})
+            if labels_value[0] == answer_value[0]:
                 ok += 1
             else:
-                # print('{:04d}: {} ({}, {})'.format(i, result[0], top_k_value, labels_value))
-                print('{:04d}: {:3d} - {:3d}'.format(i, top_value[0], labels_value[0]))
+                print('{:04d}: {:3d} - {:3d}'.format(i, answer_value[0], labels_value[0]))
                 if labels_value[0] not in errors:
                     errors[labels_value[0]] = 0
                 errors[labels_value[0]] += 1
@@ -54,6 +70,7 @@ def main(argv=None):
                     error_images.append(image_value)
         size = 10
         collage = tf.concat([tf.concat(error_images[i*size:(i+1)*size], 1) for i in range(size)], 0)
+        # collage = tf.image.convert_image_dtype(tf.div(collage, 255.0), tf.uint8)
         with open(os.path.join(os.path.dirname(__file__), 'errors.jpg'), 'wb') as f:
             f.write(sess.run(tf.image.encode_jpeg(collage)))
     print('{}/{} ({:.3f} %)'.format(ok, ok + ng, 100.0 * ok / (ok + ng)))
