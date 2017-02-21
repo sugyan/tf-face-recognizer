19a2735e16587345981659d8ffe49d38cbabd45c
sugyan <sugi1982@gmail.com>
2017-02-22 01:14:23 +0900

    update

--- eval/misc/errors.py
+++ eval/misc/errors.py
@@ -22,16 +22,14 @@ def main(argv=None):
     })
     decode = tf.image.decode_jpeg(features['image_raw'], channels=3)
     image = tf.image.resize_image_with_crop_or_pad(decode, 96, 96)
-    image = tf.image.per_image_standardization(image)
-    images = tf.expand_dims(image, axis=0)
+    inputs = tf.expand_dims(tf.image.per_image_standardization(image), axis=0)
     labels = tf.expand_dims(features['label'], axis=0)
 
     sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
     import model
-    logits = tf.nn.softmax(model.inference(images, FLAGS.num_classes))
+    logits = tf.nn.softmax(model.inference(inputs, FLAGS.num_classes))
     _, indices = tf.nn.top_k(logits)
     top = indices[0]
-    # correct = tf.nn.in_top_k(logits, labels, 1)
     # variable_averages = tf.train.ExponentialMovingAverage(model.MOVING_AVERAGE_DECAY)
 
     with tf.Session() as sess:
@@ -39,31 +37,30 @@ def main(argv=None):
         # tf.train.Saver(variable_averages.variables_to_restore()).restore(sess, FLAGS.checkpoint_path)
 
         ok, ng = 0, 0
-        error_top = {}
-        error_ans = {}
+        errors = {}
+        error_images = []
         for i, record in enumerate(tf.python_io.tf_record_iterator(FLAGS.data_file)):
-            # result = sess.run(correct, feed_dict={example: record})
-            top_value, labels_value = sess.run([top, labels], feed_dict={example: record})
-            # if result[0]:
+            top_value, labels_value, image_value = sess.run([top, labels, image], feed_dict={example: record})
             if top_value[0] == labels_value[0]:
                 ok += 1
             else:
                 # print('{:04d}: {} ({}, {})'.format(i, result[0], top_k_value, labels_value))
                 print('{:04d}: {:3d} - {:3d}'.format(i, top_value[0], labels_value[0]))
-                if top_value[0] not in error_top:
-                    error_top[top_value[0]] = 0
-                error_top[top_value[0]] += 1
-                if labels_value[0] not in error_ans:
-                    error_ans[labels_value[0]] = 0
-                error_ans[labels_value[0]] += 1
+                if labels_value[0] not in errors:
+                    errors[labels_value[0]] = 0
+                errors[labels_value[0]] += 1
                 ng += 1
+                if len(error_images) < 100:
+                    error_images.append(image_value)
+        size = 10
+        collage = tf.concat([tf.concat(error_images[i*size:(i+1)*size], 1) for i in range(size)], 0)
+        with open(os.path.join(os.path.dirname(__file__), 'errors.jpg'), 'wb') as f:
+            f.write(sess.run(tf.image.encode_jpeg(collage)))
     print('{}/{} ({:.3f} %)'.format(ok, ok + ng, 100.0 * ok / (ok + ng)))
-    print('top:')
-    for k, v in sorted(error_top.items(), key=lambda x: x[1], reverse=True):
-        print('{:3d}: {:3d}'.format(k, v))
-    print('ans:')
-    for k, v in sorted(error_ans.items(), key=lambda x: x[1], reverse=True):
-        print('{:3d}: {:3d}'.format(k, v))
+    print('errors:')
+    for k, v in sorted(errors.items(), key=lambda x: x[1], reverse=True):
+        if v >= 5:
+            print('{:3d}: {:3d}'.format(k, v))
 
 
 if __name__ == '__main__':
